1db47f40e493fdb4def1458a4a9118d9100cbd98
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-11 02:02:48 +0530

    eDabit-Solution

---
+++ array/js/18-Check-if-an-Array-Contains-a-Given-Number.js
@@ -0,0 +1,44 @@
+// Write a function to check if an array contains a particular number.
+
+// Examples
+// check([1, 2, 3, 4, 5], 3) ➞ true
+
+// check([1, 1, 2, 1, 1], 3) ➞ false
+
+// check([5, 5, 5, 6], 5) ➞ true
+
+// check([], 5) ➞ false
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+function khilji(f){
+    f = [1,2,3,4,5]
+    return f.includes(3)
+}
+document.write(khilji())
+
+document.write("<br>")
+
+function khan(j){
+    j = [1,1,2,1,1]
+    return j.includes(3)
+}
+document.write(khan())
+
+document.write("<br>")
+
+function shan(k){
+    k = [5,5,5,6]
+    return k.includes(5)
+}
+document.write(shan())
+
+document.write("<br>")
+
+function chouhan(l){
+    l = [],5
+    return l.includes()
+}
+document.write(chouhan())
