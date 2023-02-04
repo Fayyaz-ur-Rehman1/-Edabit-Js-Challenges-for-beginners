6dff2c4d5c007b361cf081788133d1bb4fc6c6c8
Fayyaz <fayyazurrehman289@gmail.com>
2023-02-05 01:40:19 +0530

    EDabit-Solution

---
+++ EDabit/js/2-pair-management.js
@@ -0,0 +1,43 @@
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+// Notes
+// N/A
+
+function makePair(arr){
+    return arr.concat()
+}
+
+console.log(makePair([1, 2]))
+console.log(makePair([51, 21]))
+console.log(makePair([512124, 215]))
+
+function arr(a,b){
+    const c = []
+    c.push(a,b)
+    return c 
+}
+console.log(arr(1, 2))
+console.log(arr(51, 21))
+console.log(arr(512124, 215))
+
+function a(s,f){
+ const a = [s,f]
+ return a
+}
+console.log(a(1,2))
+console.log(a(51,21))
+console.log(a(512124,215))
+
+function b(d,v){
+    const c = Array[d,v]
+    return c
+}
+console.log(a(1,2))
+console.log(a(51,21))
+console.log(a(512124,215))
