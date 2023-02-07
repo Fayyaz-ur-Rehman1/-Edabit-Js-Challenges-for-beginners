0f957a022b12fcfef96857e19338de6e156de735
Fayyaz <fayyazurrehman289@gmail.com>
2023-02-07 23:55:24 +0530

    EDabit-Solution

---
+++ EDabit/js/12-buggy-Code-part-3.js
@@ -0,0 +1,35 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Examples
+// sumArray([1, 2, 3, 4, 5]) ➞ 15
+
+// sumArray([-1, 0, 1]) ➞ 0
+
+// sumArray([0, 4, 8, 12]) ➞ 24
+// Notes
+// READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+// Don't overthink this challenge; it's not supposed to be hard.
+
+function sumArray(arr){
+    return arr.reduce((a,b)=> a+b)
+}
+console.log( sumArray([1, 2, 3, 4, 5]))
+console.log( sumArray([-1, 0, 1]))
+console.log( sumArray([0, 4, 8, 12]))
+
+function arr(sumArray){
+    return sumArray.map(x=>x+x)
+}
+console.log( sumArray([1, 2, 3, 4, 5]))
+console.log( sumArray([-1, 0, 1]))
+console.log( sumArray([0, 4, 8, 12]))
+
+// function a(b){
+// d = 0
+//  for(let c = 0; c < b ; c++)
+//  d+=b[c]
+// }
+// return d
+// console.log(a([1, 2, 3, 4, 5]))
+// console.log(a([-1, 0, 1]))
+// console.log(a([0, 4, 8, 12]))
